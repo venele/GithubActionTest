df0dcd281649f360a75a1206d5623d10f68e6aa4
venele <venele@users.noreply.github.com>
2021-03-27 00:40:34 +0900

    wip

---
+++ src/main/java/com/venele/trytest/TrytestApplication.java
@@ -0,0 +1,13 @@
+package com.venele.trytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrytestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/venele/trytest/TrytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venele.trytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
